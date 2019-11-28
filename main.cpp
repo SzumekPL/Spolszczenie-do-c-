a324dd348e218974b94eac4160e60f3b0d114b6a
SzumekPL <mateusz.szumilas@wp.pl>
2019-11-28 14:45:55 +0100

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include "spolszczenie.h"
+
+stala dokladna PI = 3.1415;
+
+klasa Kolo
+{
+    dokladna r;
+publiczne:
+    Kolo() : r(0.0){}
+    Kolo(dokladna _r) : r(_r){}
+    dokladna pole()
+    {
+        zwroc PI * r * r;
+    }
+};
+uzyj nazw std;
+
+start()
+{
+    rzeczywista i = 0.0;
+    napis str = "koniec";
+    Kolo* wskaznik;
+    zawsze
+    {
+        jezeli(i++ < 5)
+        {
+            wskaznik = new Kolo(dokladna(i)+2);
+            drukuj << wskaznik->pole() << nowa_linia;
+        }
+        jezeli_nie
+        {
+            drukuj << str << nowa_linia;
+            przerwij;
+        }
+    }
+    zwroc 0;
+}
---
+++ spolszczenie.h
@@ -0,0 +1,87 @@
+#ifndef SPOLSZCZENIE_H
+#define SPOLSZCZENIE_H
+
+#define zawsze while(true)
+#define dopoki while
+#define zrob do
+
+#define start() int main()
+
+#define jezeli if
+#define jezeli_nie else
+
+#define prawda true
+#define falsz false
+
+#define drukuj std::cout
+#define wypisz printf
+
+#define nowa_linia "\n"
+
+#define calkowita int
+#define rzeczywista float
+#define dokladna double
+#define napis std::string
+#define znak char
+#define puste void
+
+
+#define przerwij break
+#define kontynuuj continue
+
+#define stala const
+
+#define nieoznaczona unsigned
+#define oznaczona signed
+#define krotka short int
+#define dluga long int
+#define bardzo_dluga long long int
+
+#define stworz new
+#define usun delete
+
+#define zwroc return
+
+#define klasa class
+#define struktura struct
+
+#define domyslna default
+
+#define idz_do goto
+
+#define wylicz enum
+
+#define zaprzyjaznij friend
+#define podstaw inline
+#define plynna mutable
+#define nazw namespace
+
+#define prywatne private
+#define chronione protected
+#define publiczne public
+
+#define zarejestruj register
+
+#define rozmiar sizeof
+
+#define statyczna static
+
+#define wybor case
+#define decyzja switch
+
+#define dla for
+
+#define wzor template
+
+#define to this
+
+#define wyrzuc throw
+#define testuj try
+
+#define unia union
+
+#define uzyj using
+
+#define wirtuala virtual
+
+#endif // SPOLSZCZENIE_H
